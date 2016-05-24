d35387b3bcc1bd32f873922d8d6673a271d50666
Julian Dominguez <julido@microsoft.com>
2016-05-24 16:04:03 -0700

    Some fixes to reflection (breaking)

---
+++ src/Orleans/Shims/AssemblyLoader.cs
@@ -0,0 +1,59 @@
+#if NETSTANDARD
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Orleans.Runtime
+{
+    internal class AssemblyLoader
+    {
+        public static T TryLoadAndCreateInstance<T>(string assemblyName, Logger logger) where T : class
+        {
+            try
+            {
+                var assembly = Assembly.Load(new AssemblyName(assemblyName));
+                var foundType =
+                    TypeUtils.GetTypes(
+                        assembly,
+                        type =>
+                        typeof(T).IsAssignableFrom(type) && !type.GetTypeInfo().IsInterface
+                        && type.GetTypeInfo().GetConstructor(Type.EmptyTypes) != null, logger).FirstOrDefault();
+                if (foundType == null)
+                {
+                    return null;
+                }
+
+                return (T)Activator.CreateInstance(foundType, true);
+            }
+            catch (FileNotFoundException exception)
+            {
+                logger.Warn(ErrorCode.Loader_TryLoadAndCreateInstance_Failure, exception.Message, exception);
+                return null;
+            }
+            catch (Exception exc)
+            {
+                logger.Error(ErrorCode.Loader_TryLoadAndCreateInstance_Failure, exc.Message, exc);
+                throw;
+            }
+        }
+
+        public static T LoadAndCreateInstance<T>(string assemblyName, Logger logger) where T : class
+        {
+            try
+            {
+                var assembly = Assembly.Load(new AssemblyName(assemblyName));
+                var foundType = TypeUtils.GetTypes(assembly, type => typeof(T).IsAssignableFrom(type), logger).First();
+
+                return (T)Activator.CreateInstance(foundType, true);
+            }
+            catch (Exception exc)
+            {
+                logger.Error(ErrorCode.Loader_LoadAndCreateInstance_Failure, exc.Message, exc);
+                throw;
+            }
+        }
+    }
+}
+#endif
