e97817eebcd962841175781619bba14efce88c9c
Julian Dominguez <julido@microsoft.com>
2016-10-25 17:18:20 -0700

    Move tools outside of src

--- vNext/src/Build.cmd
+++ vNext/src/Build.cmd
@@ -54,8 +54,9 @@ if EXIST "%VERSION_FILE%" (
 @Echo PRODUCT_VERSION=%PRODUCT_VERSION%
 
 :: Restore the Tools directory to be able to restore .NET Core nuget packages
-call init-tools.cmd
-tools\dotnetcli\dotnet.exe restore
+call ..\Build\init-tools.cmd
+..\Build\tools\dotnetcli\dotnet.exe restore .
+..\Build\tools\dotnetcli\dotnet.exe restore ..\test
 
 if "%builduri%" == "" set builduri=Build.cmd
 
