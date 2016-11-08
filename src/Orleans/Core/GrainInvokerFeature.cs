001a4eaff6a82d66ccc8a960191e88103adb42b6
Julian Dominguez <julido@microsoft.com>
2016-11-08 13:47:25 -0800

    Grain reference and invoker feature

---
+++ src/Orleans/Core/GrainInvokerFeature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Orleans.CodeGeneration;
+
+namespace Orleans.Core
+{
+    /// <summary>
+    /// The collection of grain invoker types in an Orleans application. The <see cref="GrainInvokerFeature"/> can be populated
+    /// using the <see cref="ApplicationPartManager"/>.
+    /// </summary>
+    public class GrainInvokerFeature
+    {
+        /// <summary>
+        /// Gets a map from grain to grain invoker types in an Orleans application.
+        /// </summary>
+        public IDictionary<Type, TypeInfo> GrainToInvokerMap { get; } = new Dictionary<Type, TypeInfo>();
+    }
+
+    /// <summary>
+    /// Discovers grain invokers from a list of <see cref="ApplicationPart"/> instances.
+    /// </summary>
+    public class GrainInvokerFeatureProvider : IApplicationFeatureProvider<GrainInvokerFeature>
+    {
+        /// <inheritdoc />
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, GrainInvokerFeature feature)
+        {
+            foreach (var part in parts.OfType<IApplicationPartTypeProvider>())
+            {
+                foreach (var type in part.Types)
+                {
+                    var grainInvokerAttr = type.GetCustomAttribute<MethodInvokerAttribute>(false);
+                    if (grainInvokerAttr != null && !feature.GrainToInvokerMap.ContainsKey(grainInvokerAttr.TargetType))
+                    {
+                        feature.GrainToInvokerMap.Add(grainInvokerAttr.TargetType, type);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Orleans/Core/GrainReferenceFeature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Orleans.CodeGeneration;
+
+namespace Orleans.Core
+{
+    /// <summary>
+    /// The collection of grain references types in an Orleans application. The <see cref="GrainReferenceFeature"/> can be populated
+    /// using the <see cref="ApplicationPartManager"/>.
+    /// </summary>
+    public class GrainReferenceFeature
+    {
+        /// <summary>
+        /// Gets a map from grain to grain reference types in an Orleans application.
+        /// </summary>
+        public IDictionary<Type, TypeInfo> GrainToReferenceMap { get; } = new Dictionary<Type, TypeInfo>();
+    }
+
+    /// <summary>
+    /// Discovers grain references from a list of <see cref="ApplicationPart"/> instances.
+    /// </summary>
+    public class GrainRereferenceFeatureProvider : IApplicationFeatureProvider<GrainReferenceFeature>
+    {
+        /// <inheritdoc />
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, GrainReferenceFeature feature)
+        {
+            foreach (var part in parts.OfType<IApplicationPartTypeProvider>())
+            {
+                foreach (var type in part.Types)
+                {
+                    var grainReferenceAttr = type.GetCustomAttribute<GrainReferenceAttribute>(false);
+                    if (grainReferenceAttr != null && !feature.GrainToReferenceMap.ContainsKey(grainReferenceAttr.TargetType))
+                    {
+                        feature.GrainToReferenceMap.Add(grainReferenceAttr.TargetType, type);
+                    }
+                }
+            }
+        }
+    }
+}
