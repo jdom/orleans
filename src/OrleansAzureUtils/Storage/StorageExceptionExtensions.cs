4244b54a77d9137226479b93f32b0503e61da094
Julian Dominguez <julido@microsoft.com>
2017-04-21 16:28:25 -0700

    Throw InconsistentStateException when Blob storage writes with conflicting eTag (WIP)

--- src/OrleansAzureUtils/Storage/StorageExceptionExtensions.cs
+++ src/OrleansAzureUtils/Storage/StorageExceptionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table.Protocol;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
 
 namespace Orleans.Storage
 {
@@ -11,10 +12,21 @@ namespace Orleans.Storage
         /// </summary>
         internal static bool IsUpdateConditionNotSatisfiedError(this StorageException storageException)
         {
-            return storageException?.RequestInformation != null &&
-                   storageException.RequestInformation.HttpStatusCode == (int)HttpStatusCode.PreconditionFailed &&
-                   (storageException.RequestInformation.ExtendedErrorInformation == null ||
-                    storageException.RequestInformation.ExtendedErrorInformation.ErrorCode.Equals(TableErrorCodeStrings.UpdateConditionNotSatisfied));
+            return Matches(storageException, HttpStatusCode.PreconditionFailed, TableErrorCodeStrings.UpdateConditionNotSatisfied);
+        }
+
+        /// <summary>
+        /// See https://msdn.microsoft.com/en-us/library/azure/microsoft.windowsazure.storage.blob.protocol.bloberrorcodestrings.aspx
+        /// </summary>
+        internal static bool IsSourceConditionNotMetError(this StorageException storageException)
+        {
+            return Matches(storageException, HttpStatusCode.PreconditionFailed, BlobErrorCodeStrings.SourceConditionNotMet);
+        }
+
+        internal static bool Matches(this StorageException storageException, HttpStatusCode statusCode, string errorCode)
+        {
+            return storageException?.RequestInformation?.HttpStatusCode == (int)HttpStatusCode.PreconditionFailed
+                && (storageException.RequestInformation.ExtendedErrorInformation?.ErrorCode.Equals(errorCode) == true);
         }
     }
 }
