2588b8e1e58a406603441a5c556febcbcd239077
Julian Dominguez <julido@microsoft.com>
2017-09-19 15:53:52 -0700

    Add HostBuilderContext to align better with the generic HostBuilder
    Add ConfigureAppConfiguration to the host builder and context.
    ClientBuilder does not expose HostBuilderContext yet.

--- src/Orleans/Core/ClientBuilder.cs
+++ src/Orleans/Core/ClientBuilder.cs
@@ -31,14 +31,15 @@ namespace Orleans
             
             // Configure default services and build the container.
             this.serviceProviderBuilder.ConfigureServices(
-                services =>
+                (context, services) =>
                 {
                     services.TryAddSingleton(this.clientConfiguration ?? ClientConfiguration.StandardLoad());
                     services.TryAddFromExisting<IMessagingConfiguration, ClientConfiguration>();
                     services.TryAddFromExisting<ITraceConfiguration, ClientConfiguration>();
                 });
             this.serviceProviderBuilder.ConfigureServices(AddDefaultServices);
-            var serviceProvider = this.serviceProviderBuilder.BuildServiceProvider();
+
+            var serviceProvider = this.serviceProviderBuilder.BuildServiceProvider(null);
 
             serviceProvider.GetRequiredService<OutsideRuntimeClient>().ConsumeServices(serviceProvider);
 
@@ -58,7 +59,7 @@ namespace Orleans
         /// <inheritdoc />
         public IClientBuilder ConfigureServices(Action<IServiceCollection> configureServices)
         {
-            this.serviceProviderBuilder.ConfigureServices(configureServices);
+            this.serviceProviderBuilder.ConfigureServices((context, services) => configureServices(services));
             return this;
         }
 
@@ -72,11 +73,11 @@ namespace Orleans
         /// <inheritdoc />
         public IClientBuilder ConfigureContainer<TContainerBuilder>(Action<TContainerBuilder> configureContainer)
         {
-            this.serviceProviderBuilder.ConfigureContainer(configureContainer);
+            this.serviceProviderBuilder.ConfigureContainer((HostBuilderContext context, TContainerBuilder containerBuilder) => configureContainer(containerBuilder));
             return this;
         }
 
-        private static void AddDefaultServices(IServiceCollection services)
+        private static void AddDefaultServices(HostBuilderContext context, IServiceCollection services)
         {
             services.TryAddSingleton<TelemetryManager>();
             services.TryAddFromExisting<ITelemetryProducer, TelemetryManager>();
