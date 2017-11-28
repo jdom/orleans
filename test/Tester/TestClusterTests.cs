7975a6b69db863f47efc7b4f3c439f5b0b464fe3
Julian Dominguez <julido@microsoft.com>
2017-11-28 14:02:16 -0800

    Quick & dirty benchmark of silo start time

--- test/Tester/TestClusterTests.cs
+++ test/Tester/TestClusterTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,27 +12,102 @@ using TestExtensions;
 using UnitTests.GrainInterfaces;
 using UnitTests.Grains;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Tester
 {
     public class TestClusterTests : IDisposable
     {
+        private readonly ITestOutputHelper output;
         private TestCluster2 testCluster;
+
+        public TestClusterTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
 
         [Fact, TestCategory("BVT")]
         public async Task CanInitialize()
         {
-            var builder = new TestClusterBuilder(2);
+            var builder = new TestClusterBuilder(1);
+            //builder.InitializeClientOnDeploy = false;
             builder.ConfigureHostConfiguration(TestDefaultConfiguration.ConfigureHostConfiguration);
             builder.AddSiloBuilderConfigurator<TestSiloBuilderConfigurator>();
             this.testCluster = builder.Build();
 
+            var sw = Stopwatch.StartNew();
             await this.testCluster.DeployAsync();
-
+            this.output.WriteLine($"Started {this.testCluster.GetActiveSilos().Count()} and client in {sw.ElapsedMilliseconds}ms");
             var grain = this.testCluster.Client.GetGrain<ISimpleGrain>(1, SimpleGrain.SimpleGrainNamePrefix);
 
             await grain.SetA(2);
             Assert.Equal(2, await grain.GetA());
+        }
+
+        private static async Task<long?> InitializeBenchmark(int silos, bool client, ITestOutputHelper output)
+        {
+            var builder = new TestClusterBuilder(1);
+            builder.InitializeClientOnDeploy = false;
+            builder.ConfigureHostConfiguration(TestDefaultConfiguration.ConfigureHostConfiguration);
+            builder.AddSiloBuilderConfigurator<TestSiloBuilderConfigurator>();
+            var testCluster = builder.Build();
+            try
+            {
+                var sw = Stopwatch.StartNew();
+                await testCluster.DeployAsync();
+                long ellapsed = sw.ElapsedMilliseconds;
+                output.WriteLine($"Started {silos} and client={client} in {ellapsed}ms");
+                return ellapsed;
+            }
+            catch (Exception ex)
+            {
+                output.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                testCluster.StopAllSilos();
+            }
+        }
+
+        [Fact]
+        public async Task InitializeBenchmark1NoClient()
+        {
+            int count = 150;
+            await RunBenchmark(count, 1, false, this.output);
+        }
+
+        [Fact]
+        public async Task InitializeBenchmark2NoClient()
+        {
+            int count = 150;
+            await RunBenchmark(count, 2, false, this.output);
+        }
+
+        [Fact]
+        public async Task InitializeBenchmark1WithClient()
+        {
+            int count = 150;
+            await RunBenchmark(count, 1, true, this.output);
+        }
+
+
+        private async static Task RunBenchmark(int count, int silos, bool client, ITestOutputHelper output)
+        {
+            int successful = 0;
+            long totalTime = 0;
+            for (int i = 0; i < count; i++)
+            {
+                long? ellapsed = await InitializeBenchmark(silos, client, output);
+                if (ellapsed.HasValue)
+                {
+                    totalTime += ellapsed.Value;
+                    successful++;
+                }
+            }
+
+            output.WriteLine("---------------------");
+            output.WriteLine($"Average: {totalTime / count}ms (Total: {totalTime}ms / Successful: {successful})");
         }
 
         [Fact, TestCategory("BVT")]
